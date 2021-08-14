289d3c8dca31beeca6d16bf7bc34df80e3141047
Andrew Durham <andrewdurham1094@gmail.com>
2021-08-14 10:22:45 -0700

    added generator functions

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 // Assignment code here
+var buttonPush = function() {
+  
+}
+
+//generator functions
+//random lowercase
+function getRandomLC() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
+}
+
+//random uppercase
+function getRandomUC() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+//random number
+function getRandomNum() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+//random special character
+function getRandomSym() {
+  var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var randomSym = Math.floor(Math.random() * symbols.length);
+  return symbols[randomSym];
+}
+
 
 
 // Get references to the #generate element
